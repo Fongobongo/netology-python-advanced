29f3047e38876f1b52f5e274c4c2edcc002185f2
Dmitry Byankin <eblosharik@yandex.ru>
2020-04-18 11:42:55 +0500

    homework 1.2 task_1 update

--- 1.2/task_1.py
+++ 1.2/task_1.py
@@ -1,4 +1,5 @@
 import json
+import requests
 
 
 class CountriesIterator:
@@ -9,6 +10,7 @@ class CountriesIterator:
             self.json_countries = json.load(json_file)
         self.start = -1
         self.end = len(self.json_countries)
+        self.session = requests.Session()
 
     def __iter__(self):
         return self
@@ -18,7 +20,14 @@ class CountriesIterator:
         if self.start == self.end:
             raise StopIteration
         country_name = self.json_countries[self.start].get('name').get('common')
-        link = f'https://en.wikipedia.org/wiki/{country_name.replace(" ", "_")}'
+        params = {
+                "action": "opensearch",
+                "format": "json",
+                "limit": 500,
+                "search": country_name,
+        }
+        response = self.session.get('https://en.wikipedia.org/w/api.php', params=params).json()
+        link = response[3][0]
         pair = f'{country_name} - {link}'
         with open(f'{self.path[:-5]}.txt', 'a', encoding='utf-8') as links_file:
             links_file.write(f'{pair}\n')
